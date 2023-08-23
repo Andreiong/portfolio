a90882ce8028e60d05d47c45f94a3cbdf41b3356
Andrei Nowell Ong <andreinowellong@gmail.com>
2023-08-23 21:29:40 +0800

    Nav, Hero, about section

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
-import '../styles/modern-normalize.css';
-import '../styles/style.css';
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/compononents/header.css";
+import "../styles/compononents/hero.css";
+import "../styles/compononents/about.css";
+import "../styles/utils.css";
